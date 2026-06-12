67f58ca00707252ee6e8a7281186d5a06243bd9f
xmarkusai <xmarkusai@gmail.com>
2026-06-12 14:01:36 +0800

    Update submit.ts

--- api/leads/submit.ts
+++ api/leads/submit.ts
@@ -26,7 +26,7 @@ console.log("STEP 2");
 
 await transporter.sendMail({
   from: process.env.SMTP_FROM,
-  to: "markustan94@gmail.com",
+  to: "YOUR_PERSONAL_EMAIL_HERE",
   subject: "Auto Reply Test",
   text: "Auto reply test"
 });
